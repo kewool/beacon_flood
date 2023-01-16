e7fa324c020c2996ac850cb901a4e372e3f2ecb7
kewool <kkttyy324@gmail.com>
2023-01-16 15:48:48 +0900

    add option

--- main.c
+++ main.c
@@ -27,6 +27,21 @@ typedef struct {
     uint8_t rx_flags;
 } radiotap;
 
+void setRadiotap(radiotap* r, int channel) {
+    r->version = 0;
+    r->pad = 0;
+    r->length = 18;
+    r->present = 0x0000482e;
+    r->flags = 0x00;
+    r->rate = 0x82;
+    r->channel_freq = 2412 + ((channel - 1) * 5);
+    if (channel == 14) r->channel_freq = 2484;
+    r->channel_flags = 0x00a0;
+    r->antenna_signal = 0xff;
+    r->antenna = 0x00;
+    r->rx_flags = 0x0000;
+}
+
 typedef struct {
     uint8_t type;
     uint8_t flags;
@@ -37,11 +52,33 @@ typedef struct {
     uint16_t frag_seq;
 } frame;
 
+void setFrame(frame* fr) {
+  fr->type = 0x80;
+  fr->flags = 0x00;
+  fr->duration = 0x0000;
+  for(int i = 0; i < 6; i++) fr->dest[i] = 0xff;
+  int randomData = open("/dev/urandom", O_RDONLY);
+  srand(randomData);
+  for(int i = 0; i < 6; i++) {
+    int r = rand() % 256;
+    fr->src[i] = r;
+    fr->bssid[i] = r;
+  }
+  fr->frag_seq = 0x0000;
+}
+
 typedef struct {
   uint64_t timestamp;
   uint16_t interval;
   uint16_t capabilities;
 } fixed;
+
+void setFixed(fixed* fi, int crypt) {
+  fi->timestamp = 0x00000a00ab7e8183;
+  fi->interval = 0x0064;
+  fi->capabilities = 0x0401;
+  if (crypt == 1) fi->capabilities |= 0x0010;
+}
 
 typedef struct {
   uint8_t id;
@@ -53,15 +90,40 @@ typedef struct {
   wchar_t *ssid;
 } ssid;
 
+void setSSID(ssid* s, wchar_t *ssid) {
+  s->t.id = 0x00;
+  s->t.length = strlen(ssid);
+  s->ssid = ssid;
+}
+
 typedef struct {
   tag t;
   uint8_t supported_rates[8];
 } suported_rates;
 
+void setSupportedRates(suported_rates* sr) {
+  sr->t.id = 0x01;
+  sr->t.length = 8;
+  sr->supported_rates[0] = 0x82;
+  sr->supported_rates[1] = 0x84;
+  sr->supported_rates[2] = 0x8b;
+  sr->supported_rates[3] = 0x96;
+  sr->supported_rates[4] = 0x0c;
+  sr->supported_rates[5] = 0x12;
+  sr->supported_rates[6] = 0x18;
+  sr->supported_rates[7] = 0x24;
+}
+
 typedef struct {
   tag t;
   uint8_t current;
 } channel;
+
+void setChannel(channel* c, uint8_t channel) {
+  c->t.id = 0x03;
+  c->t.length = 1;
+  c->current = channel;
+}
 
 typedef struct {
   tag t;
@@ -73,6 +135,23 @@ typedef struct {
   uint16_t akm_suite_list[4];
   uint16_t rsn_capabilities;
 } rsn_info;
+
+void setRSN(rsn_info* rsn) {
+  rsn->t.id = 0x30;
+  rsn->t.length = 24;
+  rsn->rsn_version = 0x01;
+  rsn->group_cipher_suite[0] = 0x0f00;
+  rsn->group_cipher_suite[1] = 0x02ac;
+  rsn->pairwise_cipher_suite_count = 0x0002;
+  rsn->pairwise_cipher_suite_list[0] = 0x0f00;
+  rsn->pairwise_cipher_suite_list[1] = 0x04ac;
+  rsn->pairwise_cipher_suite_list[2] = 0x0f00;
+  rsn->pairwise_cipher_suite_list[3] = 0x02ac;
+  rsn->akm_suite_count = 0x0001;
+  rsn->akm_suite_list[0] = 0x0f00;
+  rsn->akm_suite_list[1] = 0x02ac;
+  rsn->rsn_capabilities = 0x000c;
+}
 
 typedef struct {
   radiotap r;
@@ -87,7 +166,8 @@ typedef struct {
 typedef struct {
   char *interface;
   wchar_t *ssid;
-  int crypt;
+  uint8_t crypt;
+  uint8_t channel;
 } args;
 
 char* str(int size) {
@@ -149,8 +229,8 @@ char** split(char *sentence, char separator) {
 
 
 void usage() {
-	printf("syntax : beacon <interface> <file>\n");
-	printf("sample : beacon wlan0 ssid.txt\n");
+	printf("syntax : beacon-flood <interface> <file> [optianl: -c, --crypt]\n");
+	printf("sample : beacon-flood wlan0 ssid.txt -c 5 --crypt -\n");
 }
 
 void sendBeacon(args* argv) {
@@ -160,67 +240,13 @@ void sendBeacon(args* argv) {
       return;
   }
   _80211* beacon = (_80211*)malloc(sizeof(_80211));
-  beacon->r.version = 0;
-  beacon->r.pad = 0;
-  // beacon->r.length = 8;
-  beacon->r.length = 18;
-  // beacon->r.present = 0x00000000;
-  beacon->r.present = 0x0000482e;
-  beacon->r.flags = 0x00;
-  beacon->r.rate = 0x82;
-  beacon->r.channel_freq = 0x096c;
-  beacon->r.channel_flags = 0x00a0;
-  beacon->r.antenna_signal = 0xff;
-  beacon->r.antenna = 0x00;
-  beacon->r.rx_flags = 0x0000;
-  beacon->fr.type = 0x80;
-  beacon->fr.flags = 0x00;
-  beacon->fr.duration = 0x0000;
-  for(int i = 0; i < 6; i++) beacon->fr.dest[i] = 0xff;
-  int randomData = open("/dev/urandom", O_RDONLY);
-  srand(randomData);
-  for(int i = 0; i < 6; i++) {
-    int r = rand() % 256;
-    beacon->fr.src[i] = r;
-    beacon->fr.bssid[i] = r;
-  }
-  printf("\n");
-  beacon->fr.frag_seq = 0x0000;
-  beacon->fi.timestamp = 0x00000a00ab7e8183;
-  beacon->fi.interval = 0x0064;
-  beacon->fi.capabilities = 0x0401;
-  if (argv->crypt == 1) beacon->fi.capabilities |= 0x0010;
-  beacon->s.t.id = 0x00;
-  beacon->s.t.length = strlen(argv->ssid);
-  beacon->s.ssid = (uint8_t*)malloc(sizeof(uint8_t) * strlen(argv->ssid));
-  memcpy(beacon->s.ssid, argv->ssid, strlen(argv->ssid));
-  beacon->sr.t.id = 0x01;
-  beacon->sr.t.length = 8;
-  beacon->sr.supported_rates[0] = 0x82;
-  beacon->sr.supported_rates[1] = 0x84;
-  beacon->sr.supported_rates[2] = 0x8b;
-  beacon->sr.supported_rates[3] = 0x96;
-  beacon->sr.supported_rates[4] = 0x0c;
-  beacon->sr.supported_rates[5] = 0x12;
-  beacon->sr.supported_rates[6] = 0x18;
-  beacon->sr.supported_rates[7] = 0x24;
-  beacon->c.t.id = 0x03;
-  beacon->c.t.length = 1;
-  beacon->c.current = 0x01;
-  beacon->rsn.t.id = 0x30;
-  beacon->rsn.t.length = 24;
-  beacon->rsn.rsn_version = 0x0001;
-  beacon->rsn.group_cipher_suite[0] = 0x0f00;
-  beacon->rsn.group_cipher_suite[1] = 0x02ac;
-  beacon->rsn.pairwise_cipher_suite_count = 0x0002;
-  beacon->rsn.pairwise_cipher_suite_list[0] = 0x0f00;
-  beacon->rsn.pairwise_cipher_suite_list[1] = 0x04ac;
-  beacon->rsn.pairwise_cipher_suite_list[2] = 0x0f00;
-  beacon->rsn.pairwise_cipher_suite_list[3] = 0x02ac;
-  beacon->rsn.akm_suite_count = 0x0001;
-  beacon->rsn.akm_suite_list[0] = 0x0f00;
-  beacon->rsn.akm_suite_list[1] = 0x02ac;
-  beacon->rsn.rsn_capabilities = 0x000c;
+  setRadiotap(&beacon->r, argv->channel);
+  setFrame(&beacon->fr);
+  setFixed(&beacon->fi, argv->crypt);
+  setSSID(&beacon->s, argv->ssid);
+  setSupportedRates(&beacon->sr);
+  setChannel(&beacon->c, argv->channel);
+  if (argv->crypt == 1) setRSN(&beacon->rsn);
 
   int size = beacon->r.length + 24 + 12 + 2 + strlen(argv->ssid) + beacon->sr.t.length + 2 + (beacon->c.t.length + 2);
   if (argv->crypt == 1) size += beacon->rsn.t.length + 2;
@@ -243,7 +269,7 @@ void sendBeacon(args* argv) {
   pcap_close(handle);
 }
 
-int main(int argc, wchar_t* argv[]) {
+int main(int argc, char* argv[]) {
   if (argc < 2) {
 		usage();
 		return -1;
@@ -272,12 +298,26 @@ int main(int argc, wchar_t* argv[]) {
   free(ssidraw);
 
   args *argvs[length];
+  uint8_t crypt = 0;
+  uint8_t channel = 1;
+  if (argc > 3) {
+    for (int j = 3; j < argc; j++) {
+      printf("%s", argv[j]);
+      if (!strcmp(argv[j], "--crypt")) crypt = 1;
+      if (!strcmp(argv[j], "-c")) channel = atoi(argv[j+1]);
+    }
+  }
+
+  char command[30];
+  sprintf(command, "iwconfig %s channel %d", argv[1], channel);
+  system(command);
+
   for(int i = 0; i < length; i++) {
     argvs[i] = (args*)malloc(sizeof(args));
     argvs[i]->ssid = ssid_list[i];
     argvs[i]->interface = argv[1];
-    if (argc == 4 && !strcmp(argv[3], "--crypt")) argvs[i]->crypt = 1;
-    else argvs[i]->crypt = 0;
+    argvs[i]->crypt = crypt;
+    argvs[i]->channel = channel;
   }
 
 
@@ -286,7 +326,7 @@ int main(int argc, wchar_t* argv[]) {
     pthread_create(&p_thread[i], NULL, sendBeacon, argvs[i]);
     pthread_detach(p_thread[i]);
   }
-  while(1) sleep(1);
+  while(1) sleep(100);
   free(ssid_list);
   return 0;
 }
